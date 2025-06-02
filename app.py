8e3eccc7d15536dcda775662ba3038f61791d843
nixonb96 <nixonbf96@gmail.com>
2025-06-02 15:58:06 -0500

    Enhance API routes for user model management by specifying HTTP methods for recommended content retrieval and clearing user models.

--- app.py
+++ app.py
@@ -113,11 +113,11 @@ def model_deployment():
 def train_model_by_user():
     return jsonify(AiComponent().main())
 
-@app.route('/recommended_content_by_user')
+@app.route('/recommended_content_by_user', methods=['GET'])
 def recommended_content_by_user():
     return jsonify(AiComponent().recommend_content())
 
-@app.route('/clear_recommended_content_by_user')
+@app.route('/clear_recommended_content_by_user', methods=['DELETE'])
 def clear_recommended_content_by_user():
     AiComponent.clear_all_models()
     return jsonify({"message": "User models cleared successfully."})
